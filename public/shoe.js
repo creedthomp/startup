009981965b935cf623a25a839fcfe8c97a74bf92
creedthomp <ctt12@byu.edu>
2023-12-12 21:17:19 -0700

    websock

--- public/shoe.js
+++ public/shoe.js
@@ -32,193 +32,64 @@ function shoemiles() {
     });
 }
 
-class Game {
-    buttons;
-    allowPlayer;
-    sequence;
-    playerPlaybackPos;
-    mistakeSound;
+class ShoeTracker {
     socket;
-  
+
     constructor() {
-      this.buttons = new Map();
-      this.allowPlayer = false;
-      this.sequence = [];
-      this.playerPlaybackPos = 0;
-      this.mistakeSound = loadSound('error.mp3');
-  
-      document.querySelectorAll('.game-button').forEach((el, i) => {
-        if (i < btnDescriptions.length) {
-          this.buttons.set(el.id, new Button(btnDescriptions[i], el));
-        }
-      });
-  
-      const playerNameEl = document.querySelector('.player-name');
-      playerNameEl.textContent = this.getPlayerName();
-  
-      this.configureWebSocket();
+        this.configureWebSocket();
+        this.checkMileageAndBroadcast();
     }
-  
-    async pressButton(button) {
-      if (this.allowPlayer) {
-        this.allowPlayer = false;
-        await this.buttons.get(button.id).press(1.0);
-  
-        if (this.sequence[this.playerPlaybackPos].el.id === button.id) {
-          this.playerPlaybackPos++;
-          if (this.playerPlaybackPos === this.sequence.length) {
-            this.playerPlaybackPos = 0;
-            this.addButton();
-            this.updateScore(this.sequence.length - 1);
-            await this.playSequence();
-          }
-          this.allowPlayer = true;
-        } else {
-          this.saveScore(this.sequence.length - 1);
-          this.mistakeSound.play();
-          await this.buttonDance(2);
-        }
-      }
-    }
-  
-    async reset() {
-      this.allowPlayer = false;
-      this.playerPlaybackPos = 0;
-      this.sequence = [];
-      this.updateScore('--');
-      await this.buttonDance(1);
-      this.addButton();
-      await this.playSequence();
-      this.allowPlayer = true;
-  
-      // Let other players know a new game has started
-      this.broadcastEvent(this.getPlayerName(), GameStartEvent, {});
-    }
-  
-    getPlayerName() {
-      return localStorage.getItem('userName') ?? 'Mystery player';
-    }
-  
-    async playSequence() {
-      await delay(500);
-      for (const btn of this.sequence) {
-        await btn.press(1.0);
-        await delay(100);
-      }
-    }
-  
-    addButton() {
-      const btn = this.getRandomButton();
-      this.sequence.push(btn);
-    }
-  
-    updateScore(score) {
-      const scoreEl = document.querySelector('#score');
-      scoreEl.textContent = score;
-    }
-  
-    async buttonDance(laps = 1) {
-      for (let step = 0; step < laps; step++) {
-        for (const btn of this.buttons.values()) {
-          await btn.press(0.0);
-        }
-      }
-    }
-  
-    getRandomButton() {
-      let buttons = Array.from(this.buttons.values());
-      return buttons[Math.floor(Math.random() * this.buttons.size)];
-    }
-  
-    async saveScore(score) {
-      const userName = this.getPlayerName();
-      const date = new Date().toLocaleDateString();
-      const newScore = { name: userName, score: score, date: date };
-  
-      try {
-        const response = await fetch('/api/score', {
-          method: 'POST',
-          headers: { 'content-type': 'application/json' },
-          body: JSON.stringify(newScore),
-        });
-  
-        // Let other players know the game has concluded
-        this.broadcastEvent(userName, GameEndEvent, newScore);
-  
-        // Store what the service gave us as the high scores
-        const scores = await response.json();
-        localStorage.setItem('scores', JSON.stringify(scores));
-      } catch {
-        // If there was an error then just track scores locally
-        this.updateScoresLocal(newScore);
-      }
-    }
-  
-    updateScoresLocal(newScore) {
-      let scores = [];
-      const scoresText = localStorage.getItem('scores');
-      if (scoresText) {
-        scores = JSON.parse(scoresText);
-      }
-  
-      let found = false;
-      for (const [i, prevScore] of scores.entries()) {
-        if (newScore > prevScore.score) {
-          scores.splice(i, 0, newScore);
-          found = true;
-          break;
-        }
-      }
-  
-      if (!found) {
-        scores.push(newScore);
-      }
-  
-      if (scores.length > 10) {
-        scores.length = 10;
-      }
-  
-      localStorage.setItem('scores', JSON.stringify(scores));
-    }
-  
-    // Functionality for peer communication using WebSocket
-  
+
+    // Functionality for WebSocket communication
     configureWebSocket() {
-      const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
-      this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
-      this.socket.onopen = (event) => {
-        this.displayMsg('system', 'game', 'connected');
-      };
-      this.socket.onclose = (event) => {
-        this.displayMsg('system', 'game', 'disconnected');
-      };
-      this.socket.onmessage = async (event) => {
-        const msg = JSON.parse(await event.data.text());
-        if (msg.type === GameEndEvent) {
-          this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
-        } else if (msg.type === GameStartEvent) {
-          this.displayMsg('player', msg.from, `started a new game`);
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+
+
+        this.socket.onopen = () => {
+            console.log('WebSocket connection established');
+        };
+
+        this.socket.onclose = () => {
+            console.log('WebSocket connection closed');
+        };
+
+        this.socket.onmessage = (event) => {
+            const msg = JSON.parse(event.data);
+            console.log(`Message received: ${msg.text}`);
+        };
+
+        this.socket.onmessage = (event) => {
+            const msg = JSON.parse(event.data);
+            const broadcastDiv = document.getElementById('broadcastMessages');
+            broadcastDiv.innerHTML += `<p>${msg.text}</p>`;  // Append new messages
+        };
+    }
+
+    // Check shoe mileage and broadcast if it hits 250 miles
+    checkMileageAndBroadcast() {
+        const shoeName = document.querySelector("#shoe").value;
+        const miles = parseFloat(document.querySelector("#miles").value);
+        const userName = localStorage.getItem("userName") ?? 'Anonymous';
+
+        if (miles >= 250) {
+            const message = `${userName}'s shoe: ${shoeName} has reached 250 miles!`;
+            this.broadcastMessage(message);
         }
-      };
     }
-  
-    displayMsg(cls, from, msg) {
-      const chatText = document.querySelector('#player-messages');
-      chatText.innerHTML =
-        `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+
+    // Broadcast a message to all connected WebSocket clients
+    broadcastMessage(message) {
+        const event = {
+            from: 'ShoeTracker',
+            text: message,
+        };
+        this.socket.send(JSON.stringify(event));
     }
-  
-    broadcastEvent(from, type, value) {
-      const event = {
-        from: from,
-        type: type,
-        value: value,
-      };
-      this.socket.send(JSON.stringify(event));
-    }
-  }
-  
-  const game = new Game();
+}
+
+// Instantiate the ShoeTracker
+const shoeTracker = new ShoeTracker();
 
 
 
